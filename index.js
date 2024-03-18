4acae1e28a2d9019b7bfd1d4ff56f3e409b66a53
DevJoseParraga <DevJoseParraga@gmail.com>
2024-03-18 10:50:14 -0300

    agregamos router, multer y pulimos los endpoints de peticiones GET y POST

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import  Express  from "express";
 const app = Express()
 app.use(Express.urlencoded({extended:true}));
 
+
 class ProductManager {
     static id = 0;
     Nombre;
@@ -17,8 +18,7 @@ class ProductManager {
         this.products = [];
         this.path = path
     }
-    
-        addProduct(title, description, price, thumbnail, code, stock, id) { // Convertir el código a un entero
+    addProduct(title, description, price, thumbnail, code, stock, id, status, category) {
         const codeAsInt = parseInt(code);
         
         if (isNaN(codeAsInt)) {
@@ -26,42 +26,87 @@ class ProductManager {
             return;
         }
     
-       
-        const existingProduct = this.products.find((prod) => prod.code === codeAsInt);
-       
-        if (existingProduct) {
-            console.log("El producto ya existe.");
-            return existingProduct;
-        }
+        let products = Productos.getProducts(); 
     
         
+    
         const newProduct = {
-            id: id !== undefined ? id : ProductManager.id, 
+            id: id !== undefined ? id : ++ProductManager.id,
             title,
             description,
             price,
             thumbnail,
             stock,
+            status,
+            category,
             code: codeAsInt,
         };
     
-      
-        this.products.push(newProduct);
+        products.push(newProduct); 
+    
+        fs.writeFileSync(this.path, JSON.stringify(products, null, 2)); 
+    
+        console.log("Producto agregado correctamente.");
+        return newProduct;
+    }
+    
+
+    
+    //     addProduct(title, description, price, thumbnail, code, stock, id) { // Convertir el código a un entero
+    //     const codeAsInt = parseInt(code);
         
-        fs.writeFileSync(this.path, JSON.stringify(this.products))
+    //     if (isNaN(codeAsInt)) {
+    //         console.log("El código debe ser un entero.");
+    //         return;
+    //     }
+    
+       
+    //     const existingProduct = this.products.find((prod) => prod.code === codeAsInt);
+       
+    //     if (existingProduct) {
+    //         console.log("El producto ya existe.");
+    //         return existingProduct;
+    //     }
+    
+        
+    //     const newProduct = {
+    //         id: id !== undefined ? id : ProductManager.id, 
+    //         title,
+    //         description,
+    //         price,
+    //         thumbnail,
+    //         stock,
+    //         code: codeAsInt,
+    //     };
+    
+       
+    //     this.products.push(newProduct);
+       
+    //     if (fs.existsSync(this.path)) {
+    //         let arr = []
+    //         let productsJson = fs.readFileSync("./products.json", "utf8")
+    //         arr = JSON.parse(productsJson)
+    //         arr.push(newProduct)
+    //         this.products.push(newProduct);
+    //         fs.writeFileSync(this.path, JSON.stringify(this.products))
+      
+
+    //     }else{
+    //         fs.writeFileSync(this.path, JSON.stringify(this.products))
+    //     }
         
     
        
-        if (id === undefined) {
-            ProductManager.id++;
-        } else {
-            console.log("Producto actualizado correctamente.");
-        }
+    //     if (id === undefined) {
+    //         ProductManager.id++;
+    //     } else {
+    //         console.log("Producto actualizado correctamente.");
+    //     }
     
         
-        console.log("Producto agregado correctamente.");
-        return newProduct; 
-    }
+    //     console.log("Producto agregado correctamente.");
+    //     return newProduct; 
+    // }
     
 
     getProducts() {
@@ -127,16 +172,9 @@ class ProductManager {
 }
  
 const Productos = new ProductManager("Tienda chino","../Desafios/products.json");
-const nuevoProducto = Productos.addProduct("Crema Corporal", "Descripción2", 2500, "linkDeLaImagen", 12343, 8, 1);
-const nuevoProducto2 = Productos.addProduct("Crema Corporal2", "Descripción2", 2500, "linkDeLaImagen2", 123410, 22, 2);
-const nuevoProducto3 = Productos.addProduct("Arroz", "Descripción3", 3090, "linkDeLaImagen3", 12345, 10, 3); 
-const nuevoProducto4 = Productos.addProduct("Pasta", "Descripción3", 2500, "linkDeLaImagen3", 12346, 10, 4);
-const nuevoProducto5 = Productos.addProduct("Leche", "Descripción3", 1000, "linkDeLaImagen3", 12347, 10, 5);
-const nuevoProducto6 = Productos.addProduct("Queso Cremoso", "Descripción2", 2000, "linkDeLaImagen", 12348, 8, 10);
-const nuevoProducto7 = Productos.addProduct("Galletitas de agua", "Descripción2", 2000, "linkDeLaImagen2", 123411, 22, 6);
-const nuevoProducto8 = Productos.addProduct("Aceitunas", "Descripción3", 3000, "linkDeLaImagen3", 12349, 10, 7); 
-const nuevoProducto9 = Productos.addProduct("Queso crema", "Descripción3", 3200, "linkDeLaImagen3", 12341, 10, 8);
-const nuevoProducto10 = Productos.addProduct("Queso azul", "Descripción3", 4000, "linkDeLaImagen3", 12342, 10, 9);
+
+
+export default Productos 
 // setTimeout(() => {
 //         console.log('=================Producto.getProducts()===================');
 //         console.log(Productos.getProducts());
@@ -162,29 +200,29 @@ const nuevoProducto10 = Productos.addProduct("Queso azul", "Descripción3", 4000
 
 //     }, 2000);
 
-    app.get("/api/products",(req, res)=>{
-        res.send(Productos.getProducts());
-    })
+//     app.get("/api/products",(req, res)=>{
+//         res.send(Productos.getProducts());
+//     })
 
-    app.get("/api/products/:idProduct",(req, res)=>{
-        const idProduct = req.params.idProduct;
-        let producto = Productos.getProductById(parseInt(idProduct))
-        if(!producto){
-            return res.send({
-                error:"usuario no encontrado"
-            }) 
-        }
-        return res.send(producto)
-    })
-    app.get("/api/product", (req, res) => {
-        const { limit } = req.query; 
-        const allProducts = Productos.getProducts(); 
-        const limitedProducts = limit ? allProducts.slice(0, parseInt(limit, 10)) : allProducts; 
-        res.send(limitedProducts);
-    });   
-const PORT = 8080;
-app.listen(PORT,()=>{
-    console.log('====================================');
-    console.log("servidor activo en http://localhost:"+PORT);
-    console.log('====================================');
-})    
+//     app.get("/api/products/:idProduct",(req, res)=>{
+//         const idProduct = req.params.idProduct;
+//         let producto = Productos.getProductById(parseInt(idProduct))
+//         if(!producto){
+//             return res.send({
+//                 error:"usuario no encontrado"
+//             }) 
+//         }
+//         return res.send(producto)
+//     })
+//     app.get("/api/product", (req, res) => {
+//         const { limit } = req.query; 
+//         const allProducts = Productos.getProducts(); 
+//         const limitedProducts = limit ? allProducts.slice(0, parseInt(limit, 10)) : allProducts; 
+//         res.send(limitedProducts);
+//     });   
+// const PORT = 8080;
+// app.listen(PORT,()=>{
+//     console.log('====================================');
+//     console.log("servidor activo en http://localhost:"+PORT);
+//     console.log('====================================');
+// })    
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,142 +1,8 @@
 import { Router } from "express";
-import fs from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Obtén la ruta al archivo actual a partir de import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-// Ahora obtén el directorio actual a partir de __filename
-const __dirname = path.dirname(__filename);
-
-// Ahora puedes construir la ruta al archivo products.json correctamente
-const productsPath = path.join(__dirname, 'products.json');
-
+import Productos from "../../index.js";
+import { uploader } from "../utils.js";
 
 const router = Router()
-class ProductManager {
-    static id = 0;
-    Nombre;
-    products;
-    path;
-
-    constructor(Nombre, path ) {
-        this.Nombre = Nombre;
-        this.products = [];
-        this.path = path
-    }
-    
-        addProduct(title, description, price, thumbnail, code, stock, id) { // Convertir el código a un entero
-        const codeAsInt = parseInt(code);
-        
-        if (isNaN(codeAsInt)) {
-            console.log("El código debe ser un entero.");
-            return;
-        }
-    
-       
-        const existingProduct = this.products.find((prod) => prod.code === codeAsInt);
-       
-        if (existingProduct) {
-            console.log("El producto ya existe.");
-            return existingProduct;
-        }
-    
-        
-        const newProduct = {
-            id: id !== undefined ? id : ProductManager.id, 
-            title,
-            description,
-            price,
-            thumbnail,
-            stock,
-            code: codeAsInt,
-        };
-    
-      
-        this.products.push(newProduct);
-        
-        fs.writeFileSync(this.path, JSON.stringify(this.products))
-        
-    
-       
-        if (id === undefined) {
-            ProductManager.id++;
-        } else {
-            console.log("Producto actualizado correctamente.");
-        }
-    
-        
-        console.log("Producto agregado correctamente.");
-        return newProduct; 
-    }
-    
-
-    getProducts() {
-        if (fs.existsSync(this.path)) {
-            let arr = []
-            let productsJson = fs.readFileSync("/products.json", "utf8")
-            arr = JSON.parse(productsJson)
-            return arr 
-        } else{
-            return this.products
-        }
-      
-    }
-
-    getProductById(id) {
-        if (fs.existsSync(this.path)) {
-            let arr = []
-            let productsJson = fs.readFileSync("/products.json", "utf8")
-            arr = JSON.parse(productsJson)
-      
-            let arrFiltrado = arr.find((prod) => prod.id === id)
-            return arrFiltrado
-        } else{
-            console.error("el producto no existe")
-        }
-         
-    }
-
-    updateProduct(id, atributo, cambio){
-        if (fs.existsSync(this.path)) {
-            let arr = []
-            let productsJson = fs.readFileSync("/products.json", "utf8")
-            arr = JSON.parse(productsJson)
-            let arrFiltrado = arr.find((prod) => prod.id === id)
-            arrFiltrado[atributo] = cambio
-            fs.unlinkSync(this.path)
-            fs.writeFileSync(this.path, JSON.stringify(arr))
-
-            return arrFiltrado[atributo]
-        } else{
-            console.error("el producto no existe")
-        }  
-    }
-    
-    
-    deleteProduct(id){
-        if (fs.existsSync(this.path)) {
-            let arr = []
-            let productsJson = fs.readFileSync("/products.json", "utf8")
-            arr = JSON.parse(productsJson)
-            let arrFiltrado = arr.filter((prod) => prod.id !== id)
-            let nuevoArr = arrFiltrado
-      
-            fs.unlinkSync(this.path)
-            
-                fs.writeFileSync(this.path, JSON.stringify(nuevoArr)) 
-                
-            
-            
-        }
-      
-        
-    }
-}
- 
-
-const Productos = new ProductManager("Tienda chino",productsPath);
-
 
 router.get("/",(req, res)=>{
     res.send(Productos.getProducts());
@@ -144,12 +10,6 @@ router.get("/",(req, res)=>{
 
 router.get("/api/products/:idProduct",(req, res)=>{
     const idProduct = req.params.idProduct;
-    console.log('================idProduct====================');
-    console.log(idProduct);
-    console.log('=================idProduct===================');
-    console.log('==================Productos==================');
-    console.log(Productos);
-    console.log('================Productos====================');
     let producto = Productos.getProductById(parseInt(idProduct))
     if(!producto){
         return res.status(400).send({
@@ -165,16 +25,32 @@ router.get("/api/product", (req, res) => {
     res.status(200).send(limitedProducts);
 }); 
 
-router.post("/", (req, res)=>{
-    const{title, description, price, thumbnail, code, stock, id } = req.body
-
-    if(!title || !description || !price || !thumbnail || !code, !stock || !id ){
+router.post("/",uploader.array("thumbnail"), (req, res)=>{
+   
+    const{title, description, price, code, stock, id, status, category } = req.body
+    const thumbnailFiles = req.files
+ 
+ 
+    if(!title || !description || !price || !thumbnailFiles  || !code || !stock || !id || !status || !category ){
         return res.status(400).send({
             error: "faltan datos para crear el producto"
         })
     }
-    Productos.addProduct(title, description, price, thumbnail, code, stock, id);
-    res.status(201).send({message: "producto creado correctamente"})
+        const thumbnail = thumbnailFiles.map(obj => obj.path)
+
+        let products = Productos.getProducts(); 
+        const existingProduct = products.find((prod) => prod.id == id);
+      
+        
+        if (existingProduct) {
+          
+            return res.status(200).send({message: `producto ${title} ya existe`})
+            ;
+        }else{
+            Productos.addProduct(title, description, price, thumbnail, code, stock, id, status, category);
+            res.status(201).send({message: `producto ${title} creado correctamente`})
+        }
+
 })
 
 export default router;
